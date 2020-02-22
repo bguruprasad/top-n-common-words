d368e5e7eff269e2bdee5a0c5fb3b39a118de776
Guruprasad Bhavsar <guruprasad.bhavsar@gmail.com>
2020-02-22 01:46:18 +0000

    Added venv and idea related files in gitignore
    Refactored code
    
    Final word_frequency.py
    
    Cleaned final code
    
    Updated Author
    
    Cleaned commented line
    
    Renamed file
    
    Added .vscode to gitignore
    
    Added exception for main
    
    Sample wiki content from page with id 21721040
    
    Added unittest for py script#
    
    added valid exception and validation function to json
    
    Corrected finally block
    
    Final Code

---
+++ word_frequency_test.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import json
+import unittest
+from collections import OrderedDict
+
+import requests
+
+import word_frequency
+
+
+class WordFrequencyTest(unittest.TestCase):
+    wiki_page_id = str(21721040)  # test wiki page id
+
+    def test_get_wiki_content(self):
+        """
+        Test for verifying json object is being returned.
+        """
+        self.assertEqual(bool(word_frequency.get_wiki_content(word_frequency.WIKI_PAGE_URL, self.wiki_page_id)), True)
+
+    def test_valid_extract_key_present(self):
+        """
+        Test for validating if json object have valid extract key present
+        """
+        self.assertTrue(word_frequency.get_wiki_content(word_frequency.WIKI_PAGE_URL, self.wiki_page_id)['query']['pages'][self.wiki_page_id]['extract'])
+
+    def test_valid_title_key_present(self):
+        """
+        Test for validating if json object have valid title key present
+        """
+        self.assertTrue(word_frequency.get_wiki_content(word_frequency.WIKI_PAGE_URL, self.wiki_page_id)['query']['pages'][self.wiki_page_id]['title'])
+
+    def test_convert_json_to_word_list(self):
+        """
+        Test to validate correct word list is being returned
+        :return:
+        """
+        word_list = self._get_word_list_from_sample_json()
+        # For testing, testing with slice of excepted word_list
+        word_list_slice = ['Stack', 'Overflow', 'question', 'answer', 'site', 'professional', 'enthusiast', 'programmers', 'privately', 'held', 'website', 'flagship', 'site', 'Stack', 'Exchange', 'Network']
+
+        self.assertListEqual(word_list[:16], word_list_slice)
+
+    def _get_word_list_from_sample_json(self):
+        """
+        Helper function to get word_list from sample data
+        :return: word_list
+        """
+        with open('sample_wiki_content.json') as wiki_json_file:
+            wiki_json = json.load(wiki_json_file)
+        word_list = word_frequency.convert_json_to_word_list(wiki_json, self.wiki_page_id)
+        return word_list
+
+    def test_get_top_n_frequent_words(self):
+        """
+        Test to validate correct top n words are returned.
+        """
+        word_list = self._get_word_list_from_sample_json()
+        original_result = word_frequency.get_top_n_frequent_words(word_list, 5)
+        excepted_result = OrderedDict()
+        for pair in [('20', ['Stack', 'questions']), ('17', ['Overflow']), ('12', ['users']), ('11', ['site', 'that']), ('10', ['question'])]:
+            excepted_result[pair[0]] = pair[1]
+
+        self.assertDictEqual(original_result, excepted_result)
+
+
+if __name__ == "__main__":
+    unittest.main()
