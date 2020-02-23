2c4a06ae197af8d2dd965bd49bf4d614523b1e83
Guruprasad Bhavsar <guruprasad.bhavsar@gmail.com>
2020-02-23 20:42:17 +0000

    Added unittest for py script#

--- word_frequency_test.py
+++ word_frequency_test.py
@@ -1,115 +1,79 @@
 #!/usr/bin/env python
-import re
-from collections import Counter, OrderedDict
+import json
+import unittest
+from collections import OrderedDict
+
 import requests
-import sys
+
+import word_frequency
 
 
-# Global Variables
-PYTHON_MAJOR_VERSION = sys.version[0]
-WIKI_PAGE_URL = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&pageids={}&explaintext&format=json"
+class WordFrequencyTest(unittest.TestCase):
+    wiki_page_id = str(21721040)  # test wiki page id
 
+    def test_get_wiki_content(self):
+        """
+        Test for verifying json object is being returned.
+        """
+        self.assertEqual(bool(word_frequency.get_wiki_content(self.wiki_page_id)), True)
 
-# Helper Functions
-def get_user_inputs():
+    def test_valid_extract_key_present(self):
+        """
+        Test for validating if json object have valid extract key present
+        """
+        self.assertTrue(word_frequency.get_wiki_content(self.wiki_page_id)['query']['pages'][self.wiki_page_id]['extract'])
+
+    def test_valid_title_key_present(self):
+        """
+        Test for validating if json object have valid title key present
+        """
+        self.assertTrue(word_frequency.get_wiki_content(self.wiki_page_id)['query']['pages'][self.wiki_page_id]['title'])
+
     """
-    Helper function to get user inputs for page_id to fetch wiki content from and words to print
-    :return wiki_page_id: page_id for wiki
-    :return words_to_print: top n frequent words to print
+    @mock.patch('word_frequency.requests.get')
+    def test_request_http_error(self, mock_get_wiki_content):
+        #Test for behaviour on http error for get_wiki_content function
+        
+        mock_response = mock.Mock()
+        http_error = requests.exceptions.HTTPError()
+        mock_response.raise_for_status.side_effect = http_error
+        mock_get_wiki_content.return_value = mock_response
+        with self.assertRaises(requests.exceptions.HTTPError):
+            word_frequency.get_wiki_content(self.wiki_page_id)
     """
-    # TODO Add exception handling
-    if PYTHON_MAJOR_VERSION == '3':
-        wiki_page_id = str(input("# Please Enter Wiki Page Id to fetch content from: "))
-        num_of_top_words = str(input("# Please Enter how many words to Frequent words to print: "))
-    elif PYTHON_MAJOR_VERSION == '2':
-        wiki_page_id = str(raw_input("# Please Enter Wiki Page Id to fetch content from: "))
-        num_of_top_words = str(raw_input("# Please Enter how many top Frequent words to print: "))
+    def test_convert_json_to_word_list(self):
+        """
+        Test to validate correct word list is being returned
+        :return:
+        """
+        word_list = self._get_word_list_from_sample_json()
+        # For testing, testing with slice of excepted word_list
+        word_list_slice = ['Stack', 'Overflow', 'question', 'answer', 'site', 'professional', 'enthusiast', 'programmers', 'privately', 'held', 'website', 'flagship', 'site', 'Stack', 'Exchange', 'Network']
 
-    return wiki_page_id, num_of_top_words
+        self.assertListEqual(word_list[:16], word_list_slice)
 
-
-def get_wiki_content(wiki_page_id):
-    """
-    Helper function to get wiki page content for page_id provided.
-    :param wiki_page_id: page_id to fetch json content from
-    :return wiki_content: json object for wiki page content
-    """
-    try:
-        wiki_content = requests.get(WIKI_PAGE_URL.format(wiki_page_id))
-        wiki_content.raise_for_status()  # This will raise the exception if status_code is not 200
-        return wiki_content.json()
-    except requests.exceptions.HTTPError as http_exception:
-        print(" [ EXCEPTION : HTTP Error Occurred : {} ]".format(str(http_exception)))
-    except requests.exceptions.ConnectionError as connection_exception:
-        print(" [ EXCEPTION : Error fetching web content : {} ]".format(str(connection_exception)))
-    except requests.exceptions.InvalidURL as invalid_url_exception:
-        print(" [ EXCEPTION : HTTP Error Occurred : {} ]".format(str(invalid_url_exception)))
-    except requests.exceptions.RequestException as request_exception:
-        print(" [ EXCEPTION : Error Occurred while serving request : {} ]".format(str(request_exception)))
-    except KeyboardInterrupt as keyboard_exception:
-        print(" [ EXCEPTION : Execution Aborted : {} ]".format(str(keyboard_exception)))
-    except Exception as generic_exception:
-        print(" [ EXCEPTION : Generic Exception Occurred : {} ]".format(str(generic_exception)))
-
-
-def convert_json_to_word_list(wiki_content_json, wiki_page_id):
-    """
-    Helper function to extract and convert.
-    :param wiki_content_json: content json to extract page content from
-    :return word_list: word_list with all required filters
-    """
-    try:
-        # extract the the content from query.pages.[wiki_page_id].extract, replace .() with space and split
-        word_list = re.sub(r'[^\w| ]', " ", wiki_content_json['query']['pages'][wiki_page_id]['extract']).split()
-
-        # strip the spliced word for possible punctuations, discard the word > 4 char length.
-        word_list = [word for word in word_list if len(word) >= 4 and not word.isnumeric()]
+    def _get_word_list_from_sample_json(self):
+        """
+        Helper function to get word_list from sample data
+        :return: word_list
+        """
+        with open('sample_wiki_content.json') as wiki_json_file:
+            wiki_json = json.load(wiki_json_file)
+        word_list = word_frequency.convert_json_to_word_list(wiki_json, self.wiki_page_id)
         return word_list
-    except Exception as e:
-        print(" [ EXCEPTION : Generic Exception Occurred : {} ]".format(str(e)))
+
+    def test_get_top_n_frequent_words(self):
+        """
+        Test to validate correct top n words are returned.
+        """
+        word_list = self._get_word_list_from_sample_json()
+        original_result = word_frequency.get_top_n_frequent_words(word_list, 5)
+        excepted_result = OrderedDict()
+        for pair in [('20', ['Stack', 'questions']), ('17', ['Overflow']), ('12', ['users']), ('11', ['that', 'site']), ('10', ['question'])]:
+            excepted_result[pair[0]] = pair[1]
+
+        self.assertDictEqual(original_result, excepted_result)
 
 
-def get_top_n_frequent_words(word_list, num_of_top_words):
-    """
-    Helper function to generate list of frequent words and return requested top n words
-    :param word_list: word_list to finding frequencies
-    :param num_of_top_words: number of top n words to print
-    :return: dictionary containing { frequencies: [word_list] }
-    """
-    try:
-        # find the frequencies of the word using collections.Counter().most_common()
-        frequencies = Counter(word_list).most_common()
-
-        # find the top n frequent words
-        i = 0
-        required_words = OrderedDict()
-        for frequency in frequencies:
-            if str(frequency[1]) in required_words:
-                required_words[str(frequency[1])].append(str(frequency[0]))
-            else:
-                if i < num_of_top_words:
-                    required_words[str(frequency[1])] = [str(frequency[0])]
-                    i += 1
-
-        return required_words
-    except Exception as e:
-        print(" [ EXCEPTION : Generic Exception Occurred : {} ]".format(str(e)))
-
-
-if __name__ == '__main__':
-    print("[ FINDING TOP N MOST FREQUENT WORDS ]")
-    wiki_page_id, num_of_top_words = get_user_inputs()
-    wiki_content_json = get_wiki_content(wiki_page_id)
-    word_list = convert_json_to_word_list(wiki_content_json, wiki_page_id)
-    result = get_top_n_frequent_words(word_list, int(num_of_top_words))
-
-    print("\nURL being called: {}".format(WIKI_PAGE_URL.format(wiki_page_id)))
-    if len(result) < int(num_of_top_words):
-        print("\nFound only top {} frequent words instead of {}.".format(str(len(result)), num_of_top_words))
-        num_of_top_words = len(result)
-
-    print("\nTitle : {}".format(wiki_content_json['query']['pages'][wiki_page_id]['title']))
-
-    print("Top {} Words:".format(str(num_of_top_words)))
-    for frequency, words in result.items():
-        print(" - {} {}".format(frequency, ', '.join(words)))
+if __name__ == "__main__":
+    unittest.main()
