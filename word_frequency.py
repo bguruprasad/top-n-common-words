daed4deb5bb553f015fa0351f1b4006fc48fd703
Guruprasad Bhavsar <guruprasad.bhavsar@gmail.com>
2020-02-24 21:30:47 +0000

    added valid exception and validation function to json

--- word_frequency.py
+++ word_frequency.py
@@ -31,28 +31,29 @@ def get_user_inputs():
     return wiki_page_id, num_of_top_words
 
 
-def get_wiki_content(wiki_page_id):
+def get_wiki_content(wiki_page_url, wiki_page_id):
     """
     Function to get wiki page content for page_id provided.
     :param wiki_page_id: page_id to fetch json content from
     :return wiki_content: json object for wiki page content
     """
+    wiki_json = {}
     try:
-        wiki_content = requests.get(WIKI_PAGE_URL.format(wiki_page_id))
+        wiki_content = requests.get(wiki_page_url.format(wiki_page_id))
         wiki_content.raise_for_status()  # This will raise the exception if status_code is not 200
-        return wiki_content.json()
+        wiki_json = wiki_content.json()
     except requests.exceptions.HTTPError as http_exception:
-        print(" [ EXCEPTION : HTTP Error Occurred : {} ]".format(str(http_exception)))
+        print("[ EXCEPTION : HTTP Error Occurred : {} ]".format(str(http_exception)))
     except requests.exceptions.ConnectionError as connection_exception:
-        print(" [ EXCEPTION : Error fetching web content : {} ]".format(str(connection_exception)))
+        print("[ EXCEPTION : Error fetching web content : {} ]".format(str(connection_exception)))
     except requests.exceptions.InvalidURL as invalid_url_exception:
-        print(" [ EXCEPTION : HTTP Error Occurred : {} ]".format(str(invalid_url_exception)))
-    except requests.exceptions.RequestException as request_exception:
-        print(" [ EXCEPTION : Error Occurred while serving request : {} ]".format(str(request_exception)))
+        print("[ EXCEPTION : HTTP Error Occurred : {} ]".format(str(invalid_url_exception)))
     except KeyboardInterrupt as keyboard_exception:
-        print(" [ EXCEPTION : Execution Aborted : {} ]".format(str(keyboard_exception)))
+        print("[ EXCEPTION : Execution Aborted : {} ]".format(str(keyboard_exception)))
     except Exception as generic_exception:
-        print(" [ EXCEPTION : Generic Exception Occurred : {} ]".format(str(generic_exception)))
+        print("[ EXCEPTION : Generic Exception Occurred : {} ]".format(str(generic_exception)))
+    finally:
+        return wiki_json
 
 
 def convert_json_to_word_list(wiki_content_json, wiki_page_id):
@@ -61,15 +62,17 @@ def convert_json_to_word_list(wiki_content_json, wiki_page_id):
     :param wiki_content_json: content json to extract page content from
     :return word_list: word_list with all required filters
     """
+    word_list = []
     try:
         # extract the the content from query.pages.[wiki_page_id].extract, replace .() with space and split
         word_list = re.sub(r'[^\w| ]', " ", wiki_content_json['query']['pages'][wiki_page_id]['extract']).split()
 
         # strip the spliced word for possible punctuations, discard the word > 4 char length.
         word_list = [word for word in word_list if len(word) >= 4 and not word.isnumeric()]
-        return word_list
     except Exception as e:
-        print(" [ EXCEPTION : Generic Exception Occurred : {} ]".format(str(e)))
+        print("[ EXCEPTION : Generic Exception Occurred : {} ]".format(str(e)))
+    finally:
+        return word_list
 
 
 def get_top_n_frequent_words(word_list, num_of_top_words):
@@ -77,7 +80,7 @@ def get_top_n_frequent_words(word_list, num_of_top_words):
     Function to generate list of frequent words and return requested top n words
     :param word_list: word_list to finding frequencies
     :param num_of_top_words: number of top n words to print
-    :return: dictionary containing { frequencies: [word_list] }
+    :return: dictionary containing { frequency: [word_list] }
     """
     try:
         # find the frequencies of the word using collections.Counter().most_common()
@@ -96,18 +99,70 @@ def get_top_n_frequent_words(word_list, num_of_top_words):
 
         return required_words
     except Exception as e:
-        print(" [ EXCEPTION : Generic Exception Occurred : {} ]".format(str(e)))
+        print("[ EXCEPTION : Generic Exception Occurred : {} ]".format(str(e)))
+        sys.exit(1)
+
+
+def check_valid_key_json(wiki_content_json, wiki_page_id):
+    """
+    Function to validate correct json keys are present.
+    :param wiki_content_json: json content in which keys to be verify
+    :param wiki_page_id: wiki page id
+    :return: True if required keys are present, else False
+    """
+    try:
+        if 'query' in wiki_content_json:
+            if 'pages' in wiki_content_json['query']:
+                if str(wiki_page_id) in wiki_content_json['query']['pages']:
+                    if 'extract' in wiki_content_json['query']['pages'][str(wiki_page_id)]:
+                        if 'title' in wiki_content_json['query']['pages'][str(wiki_page_id)]:
+                            return True
+                        else:
+                            raise KeyError('query.pages.{}.title'.format(str(wiki_page_id)))
+                    else:
+                        raise KeyError('query.pages.{}.extract'.format(str(wiki_page_id)))
+                else:
+                    raise KeyError('query.pages.{}'.format(str(wiki_page_id)))
+            else:
+                raise KeyError('query.pages')
+        else:
+            raise KeyError('query')
+    except KeyError as ke:
+        print("[ EXCEPTION : Key does not exists : {} ]".format(str(ke)))
+        return False
+    except Exception as e:
+        print("[ EXCEPTION : Generic Exception Occurred : {} ]".format(str(e)))
+        return False
 
 
 if __name__ == '__main__':
     try:
         print("[ FINDING TOP N MOST FREQUENT WORDS ]")
         wiki_page_id, num_of_top_words = get_user_inputs()
-        wiki_content_json = get_wiki_content(wiki_page_id)
-        word_list = convert_json_to_word_list(wiki_content_json, wiki_page_id)
-        result = get_top_n_frequent_words(word_list, int(num_of_top_words))
+
+        if not wiki_page_id or not num_of_top_words:
+            print('Proper input not provided, existing...')
+            exit(1)
 
         print("\nURL being called: {}".format(WIKI_PAGE_URL.format(wiki_page_id)))
+
+        wiki_content_json = get_wiki_content(WIKI_PAGE_URL, wiki_page_id)
+        if not wiki_content_json:
+            print('No JSON data return, existing...')
+            exit(1)
+
+        if not check_valid_key_json(wiki_content_json, wiki_page_id):
+            print('Unable to proceed...Exiting...')
+            exit(1)
+
+        word_list = convert_json_to_word_list(wiki_content_json, wiki_page_id)
+
+        if not word_list:
+            print('No words in the web content, unable to proceed. Existing...')
+            exit(1)
+
+        result = get_top_n_frequent_words(word_list, int(num_of_top_words))
+
         if len(result) < int(num_of_top_words):
             print("\nFound only top {} frequent words instead of {}.".format(str(len(result)), num_of_top_words))
             num_of_top_words = len(result)
@@ -118,4 +173,4 @@ if __name__ == '__main__':
         for frequency, words in result.items():
             print(" - {} {}".format(frequency, ', '.join(words)))
     except Exception as e:
-        print(" [ EXCEPTION : Generic Exception Occurred : {} ]".format(str(e)))
+        print("[ EXCEPTION : Generic Exception Occurred : {} ]".format(str(e)))
